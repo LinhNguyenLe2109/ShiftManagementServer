bc65433f56910c4689fa6d43c79d06a4cf443c4e
Anwyll Le <lelinhnguyen2109@gmail.com>
2024-02-17 19:49:13 -0500

    working on shiftSchedule

---
+++ src/database/shiftSchedule.js
@@ -0,0 +1,114 @@
+const { db } = require("./firebase.config");
+const {
+  doc,
+  setDoc,
+  getDoc,
+  query,
+  where,
+  getDocs,
+  collection,
+  deleteDoc,
+} = require("firebase/firestore");
+const logger = require("../logger");
+const { v4: uuidv4 } = require("uuid");
+const { verifyString } = require("../utils/verifyString");
+const { verifyDate } = require("../utils/verifyDate");
+const { Timestamp } = require("firebase/firestore");
+class ShiftSchedule {
+  constructor({
+    id,
+    archived,
+    shiftIdList,
+    desc,
+    startTime,
+    endTime,
+    employeeId,
+  }) {
+    this.id = id ? id : uuidv4();
+    this.desc = verifyString(desc) ? desc : "";
+    // check if the date is valid
+    // if it is, set it to the date, if not, check if the date is a string
+    // if it is, throw an error, if not, set it to the current date
+    if (verifyDate(startTime)) {
+      this.startTime = new Date(startTime);
+    } else if (verifyString(startTime)) {
+      throw new Error("Invalid date format for startTime");
+    } else {
+      throw new Error("Start time is required");
+    }
+    // Same as above
+    if (verifyDate(endTime)) {
+      this.endTime = new Date(endTime);
+    } else if (verifyString(endTime)) {
+      throw new Error("Invalid date format for endTime");
+    } else {
+      throw new Error("End time is required");
+    }
+    // employeeId is required
+    if (verifyString(employeeId)) {
+      this.employeeId = employeeId;
+    } else {
+      throw new Error("Employee ID is required");
+    }
+    this.archived = archived && typeof archived == "boolean" ? archived : false;
+    this.shifts = shiftIdList && Array.isArray(shiftIdList) ? shiftIdList : [];
+  }
+  addShiftId(shiftId) {
+    this.shifts.push(shiftId);
+  }
+  addMultipleShiftIds(shiftIdList) {
+    this.shifts = this.shifts.concat(shiftIdList);
+  }
+  removeShiftId(shiftId) {
+    this.shifts = this.shifts.filter((id) => id !== shiftId);
+  }
+  removeMultipleShiftIds(shiftIdList) {
+    this.shifts = this.shifts.filter((id) => !shiftIdList.includes(id));
+  }
+}
+// createShiftSchedule creates a new shift schedule
+// @param shiftSchedule: ShiftSchedule or object
+// @returns ShiftSchedule object
+const createShiftSchedule = async (shiftSchedule) => {
+    
+};
+
+// getShiftSchedule gets a shift schedule by its id
+// @param shiftScheduleId: string
+// @returns ShiftSchedule object
+const getShiftSchedule = async (shiftScheduleId) => {};
+
+// getShiftScheduleByDate gets a shift schedule by date (startDate >= date <= endDate)
+// @param employeeId: string
+// @param date: Date
+// @returns ShiftSchedule object
+const getShiftSchedulesByDate = async (employeeId, date) => {};
+
+// getShiftSchedules gets all shift schedules for an employee
+// @param employeeId: string
+// @returns Array of ShiftSchedule objects
+const getAllShiftSchedules = async (employeeId) => {};
+
+// updateShiftSchedule updates a shift schedule
+// To remove a shift from the shift schedule, use removeShift key
+// To remove multiple shifts from the shift schedule, use removeMultipleShifts key
+// To add a shift to the shift schedule, use addShift key
+// To add multiple shifts to the shift schedule, use addMultipleShifts key
+// @param shiftScheduleId: string
+// @param updatedShiftScheduleData: object
+const updateShiftSchedule = async (shiftScheduleId, updatedShiftScheduleData) => {};
+
+// deleteShiftSchedule deletes a shift schedule
+// @param shiftScheduleId: string
+// @returns boolean
+const deleteShiftSchedule = async (shiftScheduleId) => {};
+
+module.exports = {
+  createShiftSchedule,
+  getShiftSchedule,
+  getShiftSchedulesByDate,
+  getAllShiftSchedules,
+  updateShiftSchedule,
+  deleteShiftSchedule,
+  ShiftSchedule,
+};
---
+++ test/unit/database/shiftSchedule/createShiftSchedule.test.js
@@ -0,0 +1,8 @@
+
+
+describe("createShiftSchedule", () => {
+    test.todo("throw error if startTime is invalid");
+    test.todo("throw error if endTime is invalid");
+    test.todo("throw error if employeeId is invalid/not available");
+    test.todo("return shift schedule if create successfully");
+});
