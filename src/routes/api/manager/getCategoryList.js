139f436faed027508af2d5c46d25d8f2c0d53f23
pstaley <pstaley@myseneca.ca>
2024-03-10 15:08:44 -0400

    manager & category fixes

--- src/routes/api/manager/getCategoryList.js
+++ src/routes/api/manager/getCategoryList.js
@@ -1,5 +1,5 @@
 const { getCategory } = require("../../../database/category");
-const { getManager } = require("../../../database/manager");
+const { getUserInfo } = require("../../../database/users");
 const logger = require("../../../logger");
 
 const getCategoryList = async (req, res) => {
@@ -7,17 +7,12 @@ const getCategoryList = async (req, res) => {
     logger.info("getCategoryList function called");
     const managerId = req.params.managerId;
     logger.info("ManagerId: " + managerId);
-    const manager = await getManager(managerId);
-    logger.debug("Categories: " + JSON.stringify(manager));
-    // const categoryList = await manager.categoryList.map(async (category) => {
-    //   return await getCategory(category);
-    // });
+    const manager = await getUserInfo(managerId);
     let categoryList = [];
-    for (let i = 0; i < manager.categoryList.length; i++) {
-      const category = await getCategory(manager.categoryList[i]);
+    for (let i = 0; i < manager.accountInfo.categoryList.length; i++) {
+      const category = await getCategory(manager.accountInfo.categoryList[i]);
       categoryList.push(category.name);
     }
-    // logger.debug(categoryList);
     if (manager) {
       res.status(200).json(categoryList);
     } else {
--- src/routes/api/manager/getManager.js
+++ src/routes/api/manager/getManager.js
@@ -1,13 +1,13 @@
-const { getManager } = require("../../../database/manager");
+const { getUserInfo } = require("../../../database/users");
 const logger = require("../../../logger");
 
 const getManagerDetails = async (req, res) => {
   try {
     logger.info("getManager function called");
     const managerId = req.params.managerId;
-    const manager = await getManager(managerId);
+    const manager = await getUserInfo(managerId);
     if (manager) {
-      res.status(200).json(manager);
+      res.status(200).json(manager.accountInfo);
     } else {
       res.status(404).send("Manager not found");
     }
